ffbbf409143045606e12e72711a0e370cadc8573
Inatan <inatan_hertzog@gmail.com>
2021-05-06 22:19:15 -0300

    Complete challenge

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,5 +1,20 @@
 defmodule ListFilter do
-  def hello do
-    :world
+  require Integer
+
+  def call(list), do: count(list, 0)
+
+  defp count([], acc), do: acc
+
+  defp count([head | tail], acc) do
+    acc = head |> Integer.parse() |> is_odd(acc)
+    count(tail, acc)
   end
+
+  defp is_odd(:error, acc), do: acc
+
+  defp is_odd(tuple, acc), do: tuple |> elem(0) |> Integer.is_odd() |> validate(acc)
+
+  defp validate(true, acc), do: acc + 1
+
+  defp validate(false, acc), do: acc
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,23 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call\1" do
+    test "give a list, returns the count of odd numbers of list" do
+      response = ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"])
+      expected_response = 3
+      assert response == expected_response
+    end
+
+    test "give a list without odd numbers, returns 0" do
+      response = ListFilter.call(["banana", "6", "abc"])
+      expected_response = 0
+      assert response == expected_response
+    end
+
+    test "give a empty list, returns 0" do
+      response = ListFilter.call([])
+      expected_response = 0
+      assert response == expected_response
+    end
   end
 end
